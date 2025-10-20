1ffddc76d659f1fae3219c666b9a0f3038d87951
potlra1 <potlogradu@gmail.com>
2025-10-20 22:12:29 +0300

    Change ip

--- src/LLSketch/LLSketch.ino
+++ src/LLSketch/LLSketch.ino
@@ -14,16 +14,16 @@ struct PLCSharedVarsOutput_t
 PLCSharedVarsOutput_t& PLCOut = (PLCSharedVarsOutput_t&)m_PLCSharedVarsOutputBuf;
 
 
-AlPlc AxelPLC(1080115864);
+AlPlc AxelPLC(-2076599689);
 
 // shared variables can be accessed with PLCIn.varname and PLCOut.varname
 
 void setup()
 {
 	// Configure static IP address
-	IPAddress ip(192, 168, 1, 11);
+	IPAddress ip(192, 168, 88, 11);
 	IPAddress dns(8, 8, 8, 8);
-	IPAddress gateway(192, 168, 1, 1);
+	IPAddress gateway(192, 168, 88, 1);
 	IPAddress subnet(255, 255, 255, 0);
 	// If cable is not connected this will block the start of PLC with about 60s of timeout!
 	Ethernet.begin(ip, dns, gateway, subnet);
